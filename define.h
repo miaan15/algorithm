5c4d8955fca16acdb7b081652023ff706bed7506
miaan15 <miaanstarman@gmail.com>
2025-12-29 08:12:21 +0700

    add arraylist

---
+++ define.h
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#define I8 int8_t
+#define U8 uint8_t
+#define I32 int32_t
+#define U32 uint32_t
+#define I64 int64_t
+#define U64 uint64_t
+
+#define F32 float
+#define F64 double
+
+#define BYTE char
+#define USIZE size_t
+#define ISIZE intptr_t
