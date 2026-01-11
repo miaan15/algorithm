ef98c84ace333d3cd736994f0369f782f2566e8e
miaan15 <miaanstarman@gmail.com>
2026-01-11 14:59:23 +0700

    raylib for aabbtree demo

---
+++ aabb_tree/fetch_vendor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p ./vendor
+
+if [ ! -d "./vendor/raylib" ]; then
+    git clone --depth 1 --branch 5.5 https://github.com/raysan5/raylib.git ./vendor/raylib
+fi
+cd ./vendor/raylib/src
+make PLATFORM=PLATFORM_DESKTOP
+cd ../../..
