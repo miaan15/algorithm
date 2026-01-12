18370e9bb8e2c2224aae4c0f620e23047cb7d549
miaan15 <miaanstarman@gmail.com>
2026-01-12 11:05:54 +0700

    update aabbtree demo

--- aabb_tree/demo/main.cpp
+++ aabb_tree/demo/main.cpp
@@ -48,11 +48,37 @@ void handle_aabb_hit_bounds(AABB2f *aabb, Vec2f *velocity, float screen_width,
     }
 }
 
+void handle_aabb_pair_hitting(AABBPair const &pair, AABB2f *all_aabbs,
+                              Vec2f *all_vecs, usize cnt) {
+    int idx0 = -1, idx1 = -1;
+
+    for (usize i = 0; i < cnt; i++) {
+        if (&all_aabbs[i] == pair.first)
+            idx0 = i;
+        if (&all_aabbs[i] == pair.second)
+            idx1 = i;
+        if (idx0 != -1 && idx1 != -1)
+            break;
+    }
+
+    if (idx0 == -1 || idx1 == -1)
+        return;
+
+    float speed0 = vector_length(all_vecs[idx0]);
+    float speed1 = vector_length(all_vecs[idx1]);
+
+    all_vecs[idx0] = all_vecs[idx0] * -1.0f;
+    all_vecs[idx1] = all_vecs[idx1] * -1.0f;
+
+    all_vecs[idx0] = vector_normalize(all_vecs[idx0]) * speed1;
+    all_vecs[idx1] = vector_normalize(all_vecs[idx1]) * speed0;
+}
+
 int main(void) {
     AABBTree tree{};
     tree.margin = 5.0;
 
-    constexpr usize cnt = 3600;
+    constexpr usize cnt = 2000;
     AABB2f aabbs[cnt];
     Vec2f aabb_vecs[cnt];
 
@@ -61,9 +87,9 @@ int main(void) {
 
     for (auto i = 0; i < cnt; i++) {
         float minX = GetRandomValue(0 + 100, 1280 - 10 - 100);
-        float minY = GetRandomValue(0 + 100, 720 - 10 - 100);
-        float maxX = minX + GetRandomValue(10, 20);
-        float maxY = minY + GetRandomValue(10, 20);
+        float minY = GetRandomValue(0 + 20, 720 - 10 - 20);
+        float maxX = minX + GetRandomValue(5, 10);
+        float maxY = minY + GetRandomValue(5, 10);
 
         aabbs[i].min = {minX, minY};
         aabbs[i].max = {maxX, maxY};
@@ -94,12 +120,16 @@ int main(void) {
         aabbtree_update(&tree);
         auto pair_list = aabbtree_all_collide_pair(&tree);
 
+        for (cauto &pair : pair_list) {
+            handle_aabb_pair_hitting(pair, aabbs, aabb_vecs, cnt);
+        }
+
         std::vector<std::pair<AABB2f, usize>> debug_boxes;
-        debug_boxes_helper_recursive(&debug_boxes, tree.root, 0);
+        // debug_boxes_helper_recursive(&debug_boxes, tree.root, 0);
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        //
+
         // for (const auto &[aabb, depth] : debug_boxes) {
         //     Color border_color = debug_box_color[depth % 7];
         //     Color fill_color = border_color;
